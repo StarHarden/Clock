8129f63a21f8c902fabb546f5c0f4c5804a903d0
Star Harden <starmillerharden@gmail.com>
2020-03-21 20:41:08 -0500

    create App.js with own js30 code, modified for react.create App.css to mover css from App.js.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,61 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+//const secondHand = document.querySelector('.second-hand');
+//const minsHand = document.querySelector('.min-hand');
+//const hourHand = document.querySelector('.hour-hand');
+
+//function setDate() {
+//const now = new Date();
+
+//const seconds = now.getSeconds();
+//const secondsDegrees = ((seconds / 60) * 360) + 90;
+//secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+
+//const mins = now.getMinutess();
+//const minsDegrees = ((mins / 60) * 360) + 90;
+//minsHand.style.transform = `rotate(${minsDegrees}deg)`
+
+//const hour = now.getMinutes();
+//const hoursDegrees = ((mins / 12) * 360) + 90;
+//hourHand.style.transform = `rotate(${minsDegrees}deg)`
+//}
+//setInterval(setDate, 1000);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>JS + CSS Clock</title>
+</head>
+<body>
+
+
+    <div class="clock">
+      <div class="clock-face">
+        <div class="hand hour-hand"></div>
+        <div class="hand min-hand"></div>
+        <div class="hand second-hand"></div>
+        <div class="number number1">1</div>
+        <div class="number number2">2</div>
+        <div class="number number3">3</div>
+        <div class="number number4">4</div>
+        <div class="number number5">5</div>
+        <div class="number number6">6</div>
+        <div class="number number7">7</div>
+        <div class="number number8">8</div>
+        <div class="number number9">9</div>
+        <div class="number number10">10</div>
+        <div class="number number11">11</div>
+        <div class="number number12">12</div>
+      </div>
     </div>
+
+  
+</body>
+</html>
   );
 }
 
